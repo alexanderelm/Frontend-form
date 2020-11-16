dc2d44c2f1063fb93dac6ce2f88cc168b075a901
Alexander <alex1999@live.dk>
2020-11-16 15:19:37 +0100

    11

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,7 +1,11 @@
 $('#input-tags').tagsInput();
 
 $("a").click(function(e) {
-  e.preventDefault();
+  
   var i = $(this).data("value");
   $('#input-tags').addTag(i);
+
+  e.preventDefault();
+
+  
 });
